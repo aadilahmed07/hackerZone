86b608ca5e4832dd7f46d9dbeef30de9096a20d7
aadilahmed07 <113775853+aadilahmed07@users.noreply.github.com>
2022-11-15 13:17:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let h =(h)=>{
+    setTimeout(()=>{
+        document.getElementById("1").innerHTML="root@legend//<br>checking system information.................."
+        
+    },2000)
+    setTimeout(()=>{
+        document.getElementById("2").innerHTML="root@legend//<br>file -N tracking system.txt sample.png index.html.m@lw@re.inje(c)T{e}D"
+        
+    },5000)
+    setTimeout(()=>{
+        document.getElementById("3").innerHTML="root@legend//<br>sudo file -s /dev/sda5..m@lw@re.security.kill.capture"
+    },10000)
+    setTimeout(()=>{
+        document.getElementById("4").innerHTML=" root@legend//<br>IP Adress Captured..........<br>ping [option] [hostname_or_IP_address 0.55.21.254.254]"
+       
+    },15000)
+    setTimeout(()=>{
+        document.getElementById("5").innerHTML="root@legend//<br>Access completed...............<br>username.vap.captured{2{1}"
+    },20000)
+    setTimeout(()=>{
+        document.getElementById("6").innerHTML="root@legend//<br>Access completed...............<br>password.vap.captured{4{.2}"
+       
+    },25000)
+    setTimeout(()=>{
+        document.getElementById("7").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+       
+    },27000)
+    setTimeout(()=>{
+        document.getElementById("8").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+        
+    },29000)
+    setTimeout(()=>{
+        document.getElementById("9").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+       
+    },30000)
+    setTimeout(()=>{
+        document.getElementById("10").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+        
+    },32000)
+    setTimeout(()=>{
+        document.getElementById("11").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+       
+    },35000)
+    setTimeout(()=>{
+        document.getElementById("12").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][] <br>wait......"
+       
+    },38000)
+    setTimeout(()=>{
+        document.getElementById("13").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][] <br>wait......"
+     
+    },42000)
+    setTimeout(()=>{
+        document.body.style.backgroundImage="url('h.jpg')"
+     
+    },43000)
+    setTimeout(()=>{
+        document.getElementById("14").innerHTML="root@legend//<br>Access completed...............[][][[][][][][][][][]<br>wait......"
+        alert("warning: something went wrong")
+    },45000)
+    
+}
+h();
